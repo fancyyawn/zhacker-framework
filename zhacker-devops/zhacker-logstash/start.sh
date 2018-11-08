ed80542a2f6e8cd819c2878e2e0defe3d7b4b127
zhanghecheng <zhanghecheng@youzan.com>
2018-11-08 09:59:55 +0800

    add elk support

---
+++ zhacker-devops/zhacker-logstash/start.sh
@@ -0,0 +1,1 @@
+logstash -f ./logstash.config
--- zhacker-devops/zhacker-zipkin-server/start.sh
+++ zhacker-devops/zhacker-zipkin-server/start.sh
@@ -1,6 +1,11 @@
 # reference https://github.com/openzipkin/zipkin
 
-curl -sSL https://zipkin.io/quickstart.sh | bash -s
+if [ ! -e zipkin.jar ]
+then
+    echo 'zipkin.jar not exist , start downloading....'
+    curl -sSL https://zipkin.io/quickstart.sh | bash -s
+fi
+
 java -jar zipkin.jar
 
 # STORAGE_TYPE=elasticsearch ES_HOSTS=http://myhost:9200 java -jar zipkin.jar
