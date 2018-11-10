9ca40fa2cfa53dd1244a68dfafce77d8cc6a04a1
zhanghecheng <zhanghecheng@youzan.com>
2018-11-10 14:04:31 +0800

    add sample grey

---
+++ zhacker-samples/zhacker-sample-grey/zhacker-sample-grey-service/src/main/java/top/zhacker/sample/grey/service/rest/CRestImpl.java
@@ -0,0 +1,43 @@
+package top.zhacker.sample.grey.service.rest;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.nepxion.discovery.common.constant.DiscoveryConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import top.zhacker.boot.aop.log.ParamLog;
+
+@RestController
+@ParamLog
+@ConditionalOnProperty(name = DiscoveryConstant.SPRING_APPLICATION_NAME, havingValue = "discovery-springcloud-example-c")
+public class CRestImpl extends AbstractRestImpl {
+    private static final Logger LOG = LoggerFactory.getLogger(CRestImpl.class);
+
+    @RequestMapping(path = "/rest", method = RequestMethod.POST)
+//    @SentinelResource("sentinel-resource")
+    public String rest(@RequestBody String value) {
+        value = doRest(value);
+
+        LOG.info("调用路径：{}", value);
+
+        return value;
+    }
+
+    @RequestMapping(path = "/test", method = RequestMethod.POST)
+    public String test(@RequestBody String value) {
+        return value;
+    }
+}
