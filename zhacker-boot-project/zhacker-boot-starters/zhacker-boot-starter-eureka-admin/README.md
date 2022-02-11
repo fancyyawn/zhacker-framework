ccfc4a4661fa6039c11f5eec831aeb8176825e17
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-11 18:37:35 +0800

    Delete zhacker-boot-project directory

--- zhacker-boot-project/zhacker-boot-starters/zhacker-boot-starter-eureka-admin/README.md
+++
@@ -1,5 +0,0 @@
-# eureka-admin
-
-参考：https://github.com/SpringCloud/eureka-admin
-
-本项目主要使用starter形式引入，并基于springboot2.
--- zhacker-boot-project/zhacker-boot/src/main/java/top/zhacker/boot/aop/exception/ExceptionProcess.java
+++
@@ -1,9 +0,0 @@
-package top.zhacker.boot.aop.exception;
-
-
-public interface ExceptionProcess {
-
-  Class<?> processClass();
-
-  Object processException(Throwable e);
-}
