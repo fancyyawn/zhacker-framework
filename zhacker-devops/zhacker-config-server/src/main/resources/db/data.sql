996cd6b638c8df99803654c766c43dc4b7f59ca2
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-11 18:39:17 +0800

    Delete zhacker-devops directory

--- zhacker-devops/zhacker-config-server/src/main/resources/db/data.sql
+++
@@ -1,7 +0,0 @@
--- INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
--- VALUES ('demo', 'default', 'master', 'app.greet.name', 'Demo');
-
-
--- hystrix-dashboard
-INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
-VALUES ('zhacker-hystrix-dashboard', 'default', 'master', 'turbine.appConfig', 'retail-spu');
--- zhacker-devops/zhacker-sentinel-dashboard/start.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -e sentinel-dashboard.jar ]
-then
-    echo 'sentinel-dashboard.jar not exist , start downloading....'
-    wget http://edas-public.oss-cn-hangzhou.aliyuncs.com/install_package/demo/sentinel-dashboard.jar
-fi
-
-java -jar -Dserver.port=15000 sentinel-dashboard.jar
-
-
